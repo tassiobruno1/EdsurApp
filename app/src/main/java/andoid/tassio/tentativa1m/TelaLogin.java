0b8e8466f472eb6fe987074663f48251a58d6e2b
Tássio Bruno <tssiobruno1@gmail.com>
2023-03-02 16:03:24 -0300

    Initial commit

---
+++ app/src/main/java/andoid/tassio/tentativa1m/TelaLogin.java
@@ -0,0 +1,42 @@
+package andoid.tassio.tentativa1m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.ktx.Firebase;
+
+public class TelaLogin extends AppCompatActivity {
+
+
+    private TextView text_tela_cadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_dois);
+
+
+        getSupportActionBar().hide();
+        iniciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TelaLogin.this,FormCadastro.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+    @SuppressWarnings("unused")
+    private void IniciarComponentes() {
+    }
+
+    private void iniciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
+}
