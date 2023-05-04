18c10a0eba7ce7ccbb4bf0516f31071839f27608
Tássio Bruno <tssiobruno1@gmail.com>
2023-05-03 21:43:46 -0300

    Atualização 03/05/2023

---
+++ app/src/main/java/andoid/tassio/tentativa1m/TelaPrincipal.java
@@ -0,0 +1,45 @@
+package andoid.tassio.tentativa1m;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    private TextView nomeUsuario,emailUsuario;
+    private Button bt_deslogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+        getSupportActionBar().hide();
+        IniciarComponentes();
+
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(TelaPrincipal.this,TelaLogin.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        nomeUsuario = findViewById(R.id.textNomeUsuario);
+        emailUsuario = findViewById(R.id.textEmailUsuario);
+        bt_deslogar = findViewById(R.id.bt_deslogar);
+
+    }
+}
